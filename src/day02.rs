8f3b81cdd105649a7b2c1bbc1aae15f510928a18
webbgamers <webbgamers@gmail.com>
2021-12-02 00:21:07 -0500

    day 2 pog

--- src/day02.rs
+++ src/day02.rs
@@ -5,9 +5,40 @@ pub fn solve(input: String) -> (Option<isize>, Option<isize>) {
 }
 
 fn part1(input: &String) -> Option<isize> {
-	None
+	let input = input.lines();
+	let mut x = 0;
+	let mut d = 0;
+	for l in input {
+		let mut l = l.split_whitespace();
+		let command = l.next().unwrap();
+		let val = l.next().unwrap().parse::<usize>().unwrap();
+
+		match command {
+			"forward" => x += val,
+			"down" => d += val,
+			"up" => d -= val,
+			_ => panic!("What")
+		}
+	}
+	Option::Some((x * d) as isize)
 }
 
 fn part2(input: &String) -> Option<isize> {
-	None
+	let input = input.lines();
+	let mut x = 0;
+	let mut d = 0;
+	let mut aim = 0;
+	for l in input {
+		let mut l = l.split_whitespace();
+		let command = l.next().unwrap();
+		let val = l.next().unwrap().parse::<usize>().unwrap();
+
+		match command {
+			"forward" => { x += val; d += aim*val },
+			"down" => aim += val,
+			"up" => aim -= val,
+			_ => panic!("What")
+		}
+	}
+	Option::Some((x * d) as isize)
 }
