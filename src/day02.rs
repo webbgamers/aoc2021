ebfecbf2773dbc9ab8acd2268b8011c42c9d9119
webbgamers <webbgamers@gmail.com>
2021-12-01 20:08:58 -0500

    prepare for day 2

---
+++ src/day02.rs
@@ -0,0 +1,13 @@
+// Advent of Code 2021 Day 2 Solution by webbgamers#0246
+
+pub fn solve(input: String) -> (Option<isize>, Option<isize>) {
+	(part1(&input), part2(&input))
+}
+
+fn part1(input: &String) -> Option<isize> {
+	None
+}
+
+fn part2(input: &String) -> Option<isize> {
+	None
+}
